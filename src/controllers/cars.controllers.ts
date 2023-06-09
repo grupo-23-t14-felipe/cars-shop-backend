6db4c1005d3cd2a31fecdc4d3e8811a21fe1bcaf
Hugo Brito <hugomdmb@gmail.com>
2023-06-08 23:15:09 -0300

    create: delete car route && ensureCarExists middleware

--- src/controllers/cars.controllers.ts
+++ src/controllers/cars.controllers.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express"
 import listCarsService from "../services/cars/listCars.service"
 import { createCarService } from "../services/cars/createCars.service"
 import { updateCarService } from "../services/cars/updateCars.service"
+import { deleteCarService } from "../services/cars/deleteCars.service"
 
 export const createCarController = async (req: Request, res: Response): Promise<Response> => {
     const carCreated = await createCarService(req.body, req.user.uuid)
@@ -16,4 +17,9 @@ export const listCarsController = async (req: Request, res:Response): Promise<Re
 export const updateCarController =async (req: Request, res: Response): Promise<Response> => {
     const updatedCar = await updateCarService(req.body, req.params.uuid)
     return res.status(200).json(updatedCar)
+}
+
+export const deleteCarController =async (req: Request, res: Response): Promise<Response> => {
+    await deleteCarService(req.params.uuid)
+    return res.status(204).send()
 }
--- src/routes/cars.routes.ts
+++ src/routes/cars.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express"
-import { createCarController, listCarsController, updateCarController } from "../controllers/cars.controllers"
+import { createCarController, listCarsController, updateCarController, deleteCarController } from "../controllers/cars.controllers"
 import { validateDataMdwr } from "../middlewares/validateDataMiddleware"
 import { CarCreateRequestSchema, CarUpdateRequestSchema } from "../schemas/cars.schemas"
+import { ensureCarExistsMdwr } from "../middlewares/cars/ensureCarExists.middleware"
 
 export const carsRouter = Router()
 
 // Quando tiver a rota de login implementada, adicionar aqui autenticação no middleware, passando o UUID do usuário para o req.user
 carsRouter.post("", validateDataMdwr(CarCreateRequestSchema), createCarController)
 carsRouter.get("", listCarsController)
-carsRouter.patch("/:uuid", validateDataMdwr(CarUpdateRequestSchema), updateCarController)
+carsRouter.patch("/:uuid", ensureCarExistsMdwr, validateDataMdwr(CarUpdateRequestSchema), updateCarController)
+carsRouter.patch("/:uuid", ensureCarExistsMdwr, deleteCarController)
