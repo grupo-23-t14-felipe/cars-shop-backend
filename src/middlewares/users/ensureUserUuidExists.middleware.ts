983af4217d5883e73bbe5ad08e5f1948972d37dd
Hugo Brito <hugomdmb@gmail.com>
2023-06-14 12:56:48 -0300

    feat: add listCarsByUserId route

---
+++ src/middlewares/users/ensureUserUuidExists.middleware.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from "express";
+import { IUserRepo } from "../../interfaces/user.interface";
+import { AppDataSource } from "../../data-source";
+import { User } from "../../entities";
+import AppError from "../../errors/appError";
+
+export const ensureUserUuidExistsMdwr = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    const userRepo: IUserRepo = AppDataSource.getRepository(User)
+    const searchedUser: User | null = await userRepo.findOneBy({uuid: req.params.userUUID})
+
+    if(!searchedUser){
+        throw new AppError('User not found!', 404)
+    }
+
+    return next()
+}
