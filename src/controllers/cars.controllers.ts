5908c639074d0697a2e864323a959b20846d254c
Ana Carla <anacarlaa16@gmail.com>
2023-06-15 13:21:29 -0300

    filter added

--- src/controllers/cars.controllers.ts
+++ src/controllers/cars.controllers.ts
@@ -1,17 +1,20 @@
 import { Request, Response } from "express"
-import { listAllActiveCarsService, listCarByUserIdService } from "../services/cars/listCars.service"
 import { createCarService } from "../services/cars/createCars.service"
 import { updateCarService } from "../services/cars/updateCars.service"
 import { deleteCarService } from "../services/cars/deleteCars.service"
 import { ICar } from "../interfaces/cars.interfaces"
+import { listCarByUserIdService } from "../services/cars/listCarsByUserId.service"
+import listCarsService from "../services/cars/listCars.service"
 
 export const createCarController = async (req: Request, res: Response): Promise<Response> => {
     const carCreated = await createCarService(req.body, req.user.uuid)
     return res.status(201).json(carCreated)
 } 
 
-export const listAllCarsController = async (req: Request, res:Response): Promise<Response> =>{
-    const cars = await listAllActiveCarsService()
+export const listCarsController = async (req: Request, res:Response): Promise<Response> =>{
+    const filters = req.query
+    const page = Number(req.query.page) 
+    const cars = await listCarsService(page,filters)
     return res.status(200).json(cars)
 }
 
