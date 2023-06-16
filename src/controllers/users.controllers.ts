f8a0d99785b70dce551c9b820844efcc048e53cb
wiigny <wigny_36@hotmail.com>
2023-06-16 20:53:05 -0300

    feat: route retrieve car

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,8 +1,23 @@
-import { Request, Response } from "express"
-import { createUserService } from "../services/users/createUsers.service"
+import { Request, Response } from "express";
+import { createUserService } from "../services/users/createUsers.service";
+import { ICar } from "../interfaces/cars.interfaces";
+import { listCarByUserIdService } from "../services/users/listCarsByUserId.service";
 
-export const createUserController = async (req: Request, res: Response): Promise<Response> => {
-    const newUser = await createUserService(req.body)
-    return res.status(201).json(newUser)
-} 
+export const createUserController = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const newUser = await createUserService(req.body);
+  return res.status(201).json(newUser);
+};
 
+export const listCarByUserIdController = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const carList: ICar[] = await listCarByUserIdService(
+    req.params.userUUID,
+    req.user.uuid
+  );
+  return res.status(200).json(carList);
+};
---
+++ src/services/cars/listCarsByUuid.service.ts
@@ -0,0 +1,26 @@
+import { ICarRepo, ICarReturn } from "../../interfaces/cars.interfaces";
+import { Car } from "../../entities";
+import { AppDataSource } from "../../data-source";
+import AppError from "../../errors/appError";
+
+const retrieveCarService = async (uuid: string): Promise<Car> => {
+  const carRepository: ICarRepo = AppDataSource.getRepository(Car);
+
+  const find = await carRepository.findOne({
+    where: {
+      uuid: uuid,
+    },
+    relations: {
+      user: true,
+      gallery: true,
+    },
+  });
+
+  if (find === null) {
+    throw new AppError("Car not found.", 404);
+  }
+
+  return find;
+};
+
+export default retrieveCarService;
