187286fdef76ef71cb3822a775a798f13ad8b1f8
Ana Carla <anacarlaa16@gmail.com>
2023-06-16 16:41:46 -0300

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import 'express-async-errors'
 import express from 'express'
 import router from './routes'
+import cors from 'cors'
 
 import handdleError from './errors/handleError'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use('/',router)
 app.use(handdleError)
