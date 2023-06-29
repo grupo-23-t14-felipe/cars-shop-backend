112c0d397480de2cd3fbc02439d576450b513fb7
wiigny <wigny_36@hotmail.com>
2023-06-29 15:03:54 -0300

    feat: doc with swagger

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,13 @@ import { Router } from "express";
 import { carsRouter } from "./cars.routes";
 import { usersRouter } from "./users.routes";
 import { sessionRouter } from "./session.routes";
+import { swaggerRouter } from "./swagger.routes";
 
 const router = Router();
 
 router.use("/login", sessionRouter);
 router.use("/cars", carsRouter);
 router.use("/users", usersRouter);
+router.use("/doc", swaggerRouter);
+
 export default router;
