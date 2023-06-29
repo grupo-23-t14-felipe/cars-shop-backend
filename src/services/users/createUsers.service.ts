204050d0834d53a48ea09cf59f115833701fba18
wiigny <wigny_36@hotmail.com>
2023-06-29 16:04:26 -0300

    feat: fix create user, and changing migration

--- src/services/users/createUsers.service.ts
+++ src/services/users/createUsers.service.ts
@@ -1,5 +1,6 @@
 import { AppDataSource } from "../../data-source";
 import { User, Address } from "../../entities";
+import AppError from "../../errors/appError";
 import { IAddressRepo } from "../../interfaces/address.interfaces";
 import { IUserCreateRequest, IUserRepo, IUserResponse } from "../../interfaces/user.interface";
 import { UserCreateResponseSchema } from "../../schemas/users.schemas";
@@ -7,6 +8,10 @@ import { UserCreateResponseSchema } from "../../schemas/users.schemas";
 export const createUserService = async (userData: IUserCreateRequest): Promise<IUserResponse> => {
   const addressRepository: IAddressRepo = AppDataSource.getRepository(Address);
   const userRepository: IUserRepo = AppDataSource.getRepository(User);
+
+  const find = await userRepository.findOneBy({ email: userData.email });
+
+  if (find) throw new AppError("User already exists", 400);
 
   const newAddress = addressRepository.create(userData.address);
   const savedAddress = await addressRepository.save(newAddress);
